7866af8a22d05d065382da0c696232036e94c4e6
Phillip Youssef <Phillip Youssef@DESKTOP-9ENO0TT>
2020-12-15 22:05:40 -0600

    Add project files.

---
+++ Test 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Test_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float balance = 10;
+            float withdrawal = 30;
+            bool go;
+            if (balance >= withdrawal)
+            {
+                balance -= withdrawal;
+                go = true;
+                Console.WriteLine(balance);
+            } else
+            {
+                go = false;
+            }
+            Console.WriteLine(go);
+        }
+    }
+}
